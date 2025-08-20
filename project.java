5973221f4be67483ea64ae83734129d3705e530e
pramodcode <pramodmohanty1837@gmail.com>
2025-08-20 16:16:07 +0530

    Code

---
+++ project.java
@@ -0,0 +1,11 @@
+package hackathon;
+import java.util.Scanner;
+public class project {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to DOG BREED GYAN");
+        
+    }
+
+    
+}
